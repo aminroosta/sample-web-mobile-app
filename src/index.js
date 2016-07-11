3bb6e7ea3d6e554b14c62a43c2616e3cf97c6e45
amin roosta <amin.roosta@outlook.com>
2016-07-11 15:05:36 +0430

    setup build environment

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+import '!style!css!sass!./styles/index.scss';
+
+render(
+    <div> hello world </div>,
+  document.getElementById('root')
+)
