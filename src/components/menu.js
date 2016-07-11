6e57eacc7f5562b5f5bc9ee3cc03a1e5a5385459
amin roosta <amin.roosta@outlook.com>
2016-07-11 18:29:35 +0430

    styles for menu > user info

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import '!style!css!sass!../styles/menu.scss';
 
+const userpic = require('file!../assets/user.png');
+
 const Menu = () =>
   <div className='menu'>
-    My menu
+    <div className='info'>
+      <div className='user'>
+        <img src={userpic} className='circled' />
+
+        <span className='name'>Olia Gozha</span>
+        <span>Bet rate 116 <span className='star-icon'/></span>
+      </div>
+
+      <button> <span className='plus-icon'/> CREATE CHALLANGE </button>
+    </div>
   </div>;
 
 export default Menu;
