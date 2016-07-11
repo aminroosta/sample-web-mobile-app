6b191dfe3955ca37861e2b389cca381fe5d98a81
amin roosta <amin.roosta@outlook.com>
2016-07-11 21:07:39 +0430

    add lato font

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { render } from 'react-dom'
 /* fix tap delay on mobile browsers */
 import injectTapEventPlugin from "react-tap-event-plugin"
 injectTapEventPlugin();
+import '!style!css!../node_modules/lato-font/css/lato-font.min.css';
 import '!style!css!sass!./styles/index.scss';
 import '!style!css!../node_modules/font-awesome/css/font-awesome.min.css';
+
 
 import {controller, events} from './state.js'
 import Menu from './components/menu.js'
