07e98d7d36ddd40f5aa9e9b54085ad9a4b11b246
amin roosta <amin.roosta@outlook.com>
2016-07-11 19:58:48 +0430

    make sure sidebar menu looks great on all iphone versions (including iphone5!)

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,9 +12,16 @@ const Menu = () =>
         <span className='name'>Olia Gozha</span>
         <span>Bet rate 116 <span className='star-icon'/></span>
       </div>
-
-      <button> <i className='fa fa-plus-circle'/> CREATE CHALLANGE </button>
     </div>
+    <button> <i className='fa fa-plus-circle'/> CREATE CHALLANGE </button>
+    <ul className='links'>
+      <li> <i className='fa fa-tasks' />Activity Feed </li>
+      <li> <i className='fa fa-anchor' />My Challanges </li>
+      <li> <i className='fa fa-comments-o' />Discussions</li>
+      <li> <i className='fa fa-bar-chart' />My Stats</li>
+      <li> <i className='fa fa-user' />My Connections</li>
+      <li> <i className='fa fa-star-o' />Best Bets</li>
+    </ul>
   </div>;
 
 export default Menu;
