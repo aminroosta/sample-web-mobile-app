cc2df8ec30c5c60493c12f20cb324868bbed89dd
amin roosta <amin.roosta@outlook.com>
2016-07-11 20:57:54 +0430

    style the tapbar

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import {events} from '../state.js'
+import '!style!css!sass!../styles/home.scss';
 
 const Home = ({visible}) =>
     <div className='app'>
-        <span onTouchTap={events.toggleMenu}> toggle menu </span>
-        Home
+      <div className='topbar'>
+        <span><i className='fa fa-ellipsis-h'/> <i className='fa fa-wifi'/></span>
+        <span>4:34 PM</span>
+        <span>100% <i className='fa fa-battery-full'/></span>
+      </div>
+      <span onTouchTap={events.toggleMenu}> toggle menu </span>
     </div>;
 
 export default Home;
